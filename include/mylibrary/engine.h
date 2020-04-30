b604f7aef6f2caffffe2fad0d7e9f2f1756efd38
ericlx2 <ericlx2@illinois.edu>
2020-04-29 17:32:47 -0700

    Added proper comments

--- include/mylibrary/engine.h
+++ include/mylibrary/engine.h
@@ -1,14 +1,12 @@
-//
 // Created by Eric Xu on 4/20/20.
-//
 
 #ifndef FINALPROJECT_ENGINE_H
 #define FINALPROJECT_ENGINE_H
 
-#include <vector>
-
 #include "card.h"
 #include "player.h"
+
+#include <vector>
 
 using std::vector;
 
@@ -19,14 +17,23 @@ class Engine {
  private:
   void CreateDeck();
   void DistributeCards();
+
+  //Cards that are allowed to be used in the game
   vector<Card> cards_used;
+  //Players in the game
   vector<Player> players;
+  //Card that is currently being played
   Card played_card;
+  //Deck of cards
   vector<Card> deck;
 
+  //Total number of players in the game
   const int NUM_PLAYERS = 2;
+  //Highest amount of health each player can have
   const int MAX_HEALTH = 3;
+  //Number of cards each player draws at the beginning of their rounds
   const int DRAW_CARDS = 1;
+  //Indicates whether it is the start of the player's round
   bool start_of_round = true;
 
  public:
@@ -43,11 +50,19 @@ class Engine {
   Player& GetOpponent(int pos);
   int GetDeckSize();
 
-  int current_player = 0;
+  //Indicates whether the current state of game is for the opponent to respond
+  //to the current player
   bool responding = false;
+  //Position of the current player
+  int current_player = 0;
+  //Indicates whether the current player wants to end their round
   bool end_round = false;
+  //Indicates if the player has to discard some of their cards
   bool too_many_cards = false;
+  //Indicates whether the game is about to end
   bool end_game = false;
+
+  //The Card that has been played previously
   Card discard;
 };
 
--- src/engine.cpp
+++ src/engine.cpp
@@ -1,9 +1,6 @@
-//
 // Created by Eric Xu on 4/20/20.
-//
 
-#include "mylibrary/engine.h"
-
+#include <mylibrary/engine.h>
 #include <cinder/app/AppBase.h>
 #include <mylibrary/file_loader.h>
 
@@ -11,6 +8,14 @@
 
 namespace mylibrary {
 
+/**
+ * Constructor
+ * Initializes
+ *    1. Players in the game
+ *    2. The cards that are allowed to be used in the game
+ *    3. Creates the deck
+ *    4. Starting hand for each player
+ */
 Engine::Engine() {
   for (int i = 0; i < NUM_PLAYERS; i++) {
     Player player(MAX_HEALTH, i);
@@ -24,6 +29,9 @@ Engine::Engine() {
   DistributeCards();
 }
 
+/**
+ * Generates the deck with the cards given from the json file
+ */
 void Engine::CreateDeck() {
   for (Card card : cards_used) {
     for (int j = 0; j < card.GetNumber(); j++) {
@@ -34,6 +42,9 @@ void Engine::CreateDeck() {
       , std::mt19937(std::random_device()()));
 }
 
+/**
+ * Gives each player their starting hand
+ */
 void Engine::DistributeCards() {
   for (Player& player : players) {
     for (int i = 0; i < MAX_HEALTH; i++) {
@@ -43,6 +54,11 @@ void Engine::DistributeCards() {
   }
 }
 
+/**
+ * Main Gameplay function
+ * Reacts to player actions that are sent in from the my_app
+ * Called constantly by Update() in my_app.cc
+ */
 void Engine::PlayAction() {
   Player& player = GetPlayer(current_player);
   Player& opponent = GetOpponent(current_player);
@@ -61,6 +77,7 @@ void Engine::PlayAction() {
     }
   }
 
+  //Main functionality for when players choose cards to play
   if (responding) {
     if (opponent.PlayCard(played_card, responding)) {
       responding = false;
@@ -82,10 +99,21 @@ void Engine::PlayAction() {
   }
 }
 
+/**
+ * Gets the played card from the my_app when user clicks on a card
+ * @param card: card played
+ */
 void Engine::PlayCard(Card& card) {
   played_card = card;
 }
 
+/**
+ * Checks whether current player has too many cards
+ * in hand when ending their round
+ * If the player has more card than current health, then they are required to
+ * discard x amount of cards
+ * @param card: card being discarded
+ */
 void Engine::EndRound(Card& card) {
   Player& player = GetPlayer(current_player);
   player.RemoveCard(card);
@@ -96,6 +124,9 @@ void Engine::EndRound(Card& card) {
   }
 }
 
+/**
+ * Switches player turns when current player decides to end their round
+ */
 void Engine::SwitchPlayer() {
   start_of_round = true;
   discard.reset();
@@ -106,6 +137,10 @@ void Engine::SwitchPlayer() {
   }
 }
 
+/**
+ * Called when a player tries to "hit" their opponent and their opponent does
+ * not have "dodge"
+ */
 void Engine::UnableToDodge() {
   Player& opponent = GetOpponent(current_player);
   responding = false;
@@ -113,6 +148,12 @@ void Engine::UnableToDodge() {
   discard.reset();
 }
 
+/**
+ * Checks whether the whole game is about to end
+ * The only way for a game to end is:
+ *        1. The player has 0 health
+ *        2. The player does not have a "heal" card to heal back up
+ */
 void Engine::CheckEndGame() {
   for (Player& player : players) {
     if (player.GetHealth() == 0) {
@@ -123,6 +164,12 @@ void Engine::CheckEndGame() {
   }
 }
 
+/**
+ * Called when a player has 0 health
+ * This is the player's last chance to not lose
+ * The player is allowed to play only the "heal" card
+ * If not, then game over
+ */
 void Engine::Save() {
   if (GetPlayer(current_player).Save(played_card)) {
     discard = played_card;
@@ -132,10 +179,16 @@ void Engine::Save() {
   }
 }
 
+/**
+ * Gets reference to the current player
+ */
 Player& Engine::GetPlayer(int pos) {
   return players[pos];
 }
 
+/**
+ * Gets reference to the opposing player
+ */
 Player& Engine::GetOpponent(int pos) {
   if (pos == 0) {
     return players[1];
@@ -143,6 +196,9 @@ Player& Engine::GetOpponent(int pos) {
   return players[0];
 }
 
+/**
+ * Gets the deck's current size
+ */
 int Engine::GetDeckSize() {
   return deck.size();
 }
